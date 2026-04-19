9d61ae3cc5f20a884f2eeaf41a284dd18d30b53c
PrincessAnnNicolas <2023101622@ms.bulsu.edu.ph>
2026-04-19 15:57:40 +0800

    sidebar and mobile header clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,14 @@ import Schedule from './pages/Schedule';
 import LandingPage from './pages/LandingPage';
 import Aboutus from './pages/Aboutus';
 import Signup from './pages/Signup';
+import Dashboard from './pages/dashboard';
+import Analytics from './pages/analytics';
+import Management from './pages/management';
+import Tracking from './pages/Tracking';
 
 
 function App() {
-  return <Signup/>
+  return <Dashboard/>
 }
 
 export default App;
