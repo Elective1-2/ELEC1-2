0f9a7ceb178d05322e8aa85fcfa4ab4052921814
Nicolas, Princess Ann S. <2023101622@ms.bulsu.edu.ph>
2026-04-19 14:22:10 +0800

    Signup (#17)
    * tyL
    
    * fixing tracking.css
    
    * fixed tracking.css
    
    * Signup css and jsx
    
    ---------
    
    Co-authored-by: CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Schedule from './pages/Schedule';
 import LandingPage from './pages/LandingPage';
+import Aboutus from './pages/Aboutus';
+import Signup from './pages/Signup';
+
 
 function App() {
-  return <LandingPage/>
+  return <Signup/>
 }
 
 export default App;
