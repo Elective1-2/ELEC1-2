dee8001f63f1e686b0d75f86493980a9175f2acb
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-20 22:40:05 +0800

    deployment test: express

--- backend/src/test.js
+++ backend/src/test.js
@@ -1,3 +1,11 @@
-const http = require('http');
-const server = http.createServer((req, res) => res.end('OK'));
-server.listen(process.env.PORT || 3000, () => console.log('Test server running'));
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Express is working!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Express server running on port ${PORT}`);
+});
