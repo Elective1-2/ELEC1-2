d1aff403fca17ec7831093ac29cc371fd80f6afd
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-20 10:03:58 +0800

    Added routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';
+
+console.log('main.jsx is running');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
