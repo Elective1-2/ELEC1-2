d4c5aa520ccaf165e15e18939294131b1ff11760
Tubuan, Sophia Marielle V. <2023106066@ms.bulsu.edu.ph>
2026-04-20 19:58:08 +0800

    Driver Main and Driver Map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Schedule from './pages/Schedule';
+import LandingPage from './pages/LandingPage';
+import Aboutus from './pages/Aboutus';
+import Login from './pages/login';
+import M2BDashboard from './pages/dashboard';
+import Tracking from './pages/Tracking';
+import BarChart from './pages/analytics';
+import Management from './pages/management';
+import tripData from './pages/passenger';
+import DriverMain from './pages/drivermain';
+import DriverMap from './pages/drivermap';
+
 
 function App() {
-  return <Schedule/>
+  return <DriverMain />
 }
 
 export default App;
