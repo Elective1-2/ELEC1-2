476c0559f18673c5e8ae2459de058c67dde3e10d
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-24 04:02:32 +0800

    API hardcoded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/app.routes';
 
 function App() {
-  const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_ID = "824368345277-d9bubjeb9hdbbbs33eaj6117l4qu40qk.apps.googleusercontent.com";
 
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
