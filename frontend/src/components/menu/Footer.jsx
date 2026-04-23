b91640a00393dba45ea6dbb4689403e85e4a56e8
Nicolas, Princess Ann S. <2023101622@ms.bulsu.edu.ph>
2026-04-23 10:57:55 +0800

    Footer okay na (#45)

--- frontend/src/components/menu/Footer.jsx
+++ frontend/src/components/menu/Footer.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import '../../css/Footer.css';
-import logo from '../../assets/M2B logo.png';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        {/* Logo and Name Section */}
-        <div className="footer-branding">
-          <img src={logo} alt="M2B Logo" className="footer-logo-img" />
+        {/* Credits Section */}
+        <div className="footer-info">
+          <span className="credit-text">Developed by <strong>BSCpE Students</strong></span>
+          <span className="separator">|</span>
+          <span className="credit-text">Bulacan State University</span>
         </div>
 
-        {/* Development Credits Section */}
-        <div className="footer-credits">
-          <p className="credit-text">Developed by <strong>BSCpE Students</strong></p>
-          <p className="credit-text">Bulacan State University</p>
-        </div>
-
-        <div className="footer-divider"></div>
-
-        {/* Copyright Section */}
+        {/* Copyright Section - Below */}
         <div className="footer-copyright">
           <p>&copy; 2026 M2B. All Rights Reserved.</p>
         </div>
