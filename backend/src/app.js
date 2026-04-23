a1ac2c001c4d1b0184117cd5600b190bc1a965e6
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-24 06:02:42 +0800

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -99,7 +99,7 @@ app.get('/health', (req, res) => {
 
 // Only serve static files in production
   if (process.env.NODE_ENV === 'production') {
-    const frontendBuildPath = path.join(__dirname, '../../public_html/dist');
+    const frontendBuildPath = path.join(__dirname, '../dist');
     
     // Add this debug code
     const fs = require('fs');
