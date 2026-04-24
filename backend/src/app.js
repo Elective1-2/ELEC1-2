3513b5b801487fdf7a8362378de6d1484860ea0f
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-24 13:50:06 +0800

    rate limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -84,8 +84,8 @@ console.log('✅ [app.js] Helmet configured');
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 1 * 60 * 1000,
+  max: 10000,
   message: { error: 'Too many requests, please try again later.' },
   standardHeaders: true,
   legacyHeaders: false,
